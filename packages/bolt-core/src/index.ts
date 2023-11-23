de5185826b04fee3815278fd77226c4d0d8b07c6
Nico <contact@itsrems.com>
2023-11-23 11:46:44 +0000

    name optional

--- packages/bolt-core/src/index.ts
+++ packages/bolt-core/src/index.ts
@@ -16,6 +16,6 @@ declare type Tagged<Token> = {
 
 export type Opaque<Type, Token = unknown> = Type & Tagged<Token>;
 
-export function createRouter<N extends string, T extends RouterRecord>(name: N, router: T): Opaque<T, N> {
+export function createRouter<N extends string, T extends RouterRecord>(router: T, name?: N): Opaque<T, N> {
   return router as Opaque<T, N>;
 }
