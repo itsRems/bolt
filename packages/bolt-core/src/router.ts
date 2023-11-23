ffacbe989727607ac4f76393c34a9ffad23db9aa
Nico <contact@itsrems.com>
2023-11-23 12:42:39 +0000

    revert

--- packages/bolt-core/src/router.ts
+++ packages/bolt-core/src/router.ts
@@ -1,13 +1,9 @@
 import { AnyRoute } from './route';
 
-export type NestedRouterRecord<T> = {
-  [K in keyof T]: T[K] extends AnyRoute | infer U ? U extends RouterRecord ? NestedRouterRecord<U> : U : never;
-};
-
 export interface RouterRecord {
   [key: string]: AnyRoute | RouterRecord;
 }
 
-export function createRouter<T extends RouterRecord>(router: T): NestedRouterRecord<T> {
-  return router as any;
+export function createRouter<T extends RouterRecord>(router: T) {
+  return router;
 }
